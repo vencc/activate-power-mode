26982b8849420f6144bf78ae71be4144c5c4865d
vencc__ <2641150756@qq.com>
2018-03-30 22:50:08 +0800

    修改使用精圆后锯齿没设置回导致不能设置回粗圆的bug

--- src/com/jiyuanime/shape/RoughRoundShape.java
+++ src/com/jiyuanime/shape/RoughRoundShape.java
@@ -10,6 +10,7 @@ import java.awt.*;
  */
 public class RoughRoundShape {
     public static Graphics2D getGraphics(Graphics2D g,int x,int y,int size,Color c){
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
         g.setColor(c);
         g.fillOval(x, y, size, size);
         return g;
